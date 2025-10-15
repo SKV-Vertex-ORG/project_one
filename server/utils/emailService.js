4f990932c1d005080caf495ad33b65956999b911
sanjay-kv2769 <93617720+sanjay-kv2769@users.noreply.github.com>
2025-10-16 01:28:50 +0530

    Save current work before reverting

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -12,7 +12,7 @@ class EmailService {
       return null;
     }
 
-    return nodemailer.createTransporter({
+    return nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
       secure: false, // true for 465, false for other ports
