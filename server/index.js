d4ddd8528839972e7ef4dd0bd006b8475c80c51f
sanjay-kv2769 <93617720+sanjay-kv2769@users.noreply.github.com>
2025-10-16 00:34:12 +0530

    deployment configs

--- server/index.js
+++ server/index.js
@@ -20,7 +20,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://vertex-app.vercel.app'
   ],
   credentials: true
 }));
